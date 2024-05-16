112b305d901dc57f048b07b2f73a9c48096dfce7
Andrew <andrew@Capelagames.com.au>
2024-05-16 12:55:28 +1000

    highscore

---
+++ Assets/Scripts/Saving/HighScoreData.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[System.Serializable]
+public class HighScoreData
+{
+    public float[] scores;
+    public string[] names; 
+
+    public HighScoreData()
+    {
+        scores = new []{99f, 40f, 2f};
+        names = new []{"Andrew", "Me Please" , "Adam"};
+    }
+
+    public HighScoreData(float[] scores, string[] names)
+    {
+        this.scores = scores;
+        this.names = names;
+    }
+}
+
+[System.Serializable]
+public class Float3
+{
+    public float x, y, z;
+
+    public Float3(Vector3 vector)
+    {
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+
+    Vector3 ToVector()
+    {
+        return new Vector3(x, y, z);
+    }
+}
