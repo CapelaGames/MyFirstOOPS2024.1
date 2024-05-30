aa68b9ba86c41ae51e9dbbbb5aacb7e83cc1b4f7
Andrew <andrew@Capelagames.com.au>
2024-05-30 11:13:48 +1000

    upgrade disable enable buttons

--- Assets/Scripts/Upgrades/BaseUpgrade.cs
+++ Assets/Scripts/Upgrades/BaseUpgrade.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public abstract class BaseUpgrade 
+public abstract class BaseUpgrade
 {
+    public Button upgradeButton;
+    
     protected int cost = 1;
 
     public abstract void ApplyUpgrade();
 
     public abstract string UpgradeName();
+
+    public void SetButton(Button button)
+    {
+        upgradeButton = button;
+    }
+    
+    public void CheckButtonCost(int money)
+    {
+        if (upgradeButton == null)
+        {
+            return;
+        }
+
+        if (money < cost)
+        {
+            upgradeButton.interactable = false;
+        }
+        else
+        {
+            upgradeButton.interactable = true;
+        }
+    }
     
     public bool PayForUpgrade(ref int money)
     {
--- Assets/Scripts/Upgrades/UpgradeManager.cs
+++ Assets/Scripts/Upgrades/UpgradeManager.cs
@@ -6,24 +6,43 @@ using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour
 {
+    //Field
     [SerializeField] private int money = 0;
     [SerializeField] private Button buttonPrefab;
     [SerializeField] private Transform buttonParent;
     private PlaneController planeController;
     private List<BaseUpgrade> upgrades;
+    
+    //property
+    public int Money
+    {
+        get
+        {
+            return money;
+        }
+        set
+        {
+            money = value;
+            foreach (var upgrade in upgrades)
+            {
+                upgrade.CheckButtonCost(value);
+            }
+        }
+    }
+    
     void Start()
     {
         planeController = FindObjectOfType<PlaneController>();
         
         upgrades = new List<BaseUpgrade>();
-        /*upgrades.Add(new SpeedUpgrade(planeController, 5, 500f));
+        upgrades.Add(new SpeedUpgrade(planeController, 5, 500f));
         upgrades.Add(new SpeedUpgrade(planeController, 8, 1000f));
         upgrades.Add(new SpeedUpgrade(planeController, 12, 1500f));
         upgrades.Add(new SpeedUpgrade(planeController, 15, 2000f));
         upgrades.Add(new GravityUprade(planeController, 5, 0.5f));
         upgrades.Add(new GravityUprade(planeController, 8, 1f));
         upgrades.Add(new GravityUprade(planeController, 12, 1.5f));
-        upgrades.Add(new GravityUprade(planeController, 15, 2f));*/
+        upgrades.Add(new GravityUprade(planeController, 15, 2f));
         
        int x = 0;
         foreach (var upgrade in upgrades)
@@ -41,10 +60,17 @@ public class UpgradeManager : MonoBehaviour
 
     public void PurchaseUpgrade(int index)
     {
-        if (upgrades[index].PayForUpgrade(ref money))
+        int tempMoney = Money;
+        if (upgrades[index].PayForUpgrade(ref tempMoney))
         {
             upgrades[index].ApplyUpgrade();
         }
 
+        Money = tempMoney;
+    }
+
+    public void EarnMoney(int amount)
+    {
+        Money += amount;
     }
 }
